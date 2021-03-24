a94bfd16cdc6af3edb9a07b2be820abe013d5c57
Vera <55832724+potatbut@users.noreply.github.com>
2021-03-24 19:08:50 +0300

    Resize table (#5)
    * Finished resizing element
    
    * Refactoring the resize function

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,48 +1,26 @@
 import {ExcelComponent} from '@core/ExcelComponent';
+import {needToResize} from './table.function';
+import {resizeHandler} from './table.resize';
 import {createTable} from './table.template';
+/* import {$} from '@core/dom' */
 
 export class Table extends ExcelComponent {
   static className = 'table'
 
+  constructor($root) {
+    super($root, {
+      name: 'Table',
+      listeners: ['mousedown'],
+    })
+  }
+
   toHTML() {
     return createTable()
   }
+
+  onMousedown(event) {
+    if (needToResize(event)) {
+      resizeHandler(this.$root, event)
+    }
+  }
 }
-
-/* `
-      <div class="row">
-
-        <div class="row-info"></div>
-
-        <div class="row-data">
-            <div class="column">A</div>
-            <div class="column">B</div>
-            <div class="column">C</div>
-        </div>
-
-      </div>
-
-      <div class="row">
-
-          <div class="row-info">1</div>
-
-          <div class="row-data">
-              <div class="cell" contenteditable>fdg g</div>
-              <div class="cell" contenteditable>1dffg</div>
-              <div class="cell" contenteditable>dfgg</div>
-          </div>
-
-      </div>
-
-      <div class="row">
-
-          <div class="row-info">2</div>
-
-          <div class="row-data">
-              <div class="cell selected">fdg g</div>
-              <div class="cell">1dffg</div>
-              <div class="cell">dfgg</div>
-          </div>
-
-      </div>
-    ` */
